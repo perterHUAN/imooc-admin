a5533f825c2a861fb9543d4449ade3083b47a61f
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-06-23 10:16:56 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <el-button type="primary">你好</el-button>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
